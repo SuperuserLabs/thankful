9dd107679cc7d05df9b9c850bfe2829f1ac25885
Johannes Ahnlide <johannes@voxel.se>
2018-06-11 14:51:30 +0200

    Removed tests

--- src/background.test.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert');
-const Background = require('../background.js');
-
-describe('Background', () => {
-  it('has a test', () => {
-    assert(false, 'Background should have a test');
-  });
-});

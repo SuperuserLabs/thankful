752ebbf17c59737cc2ea77a7c25e87c418fec60c
Johannes Ahnlide <johannes@voxel.se>
2018-06-11 14:31:49 +0200

    Added dummy tests

---
+++ src/background.test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+const Background = require('../background.js');
+
+describe('Background', () => {
+  it('has a test', () => {
+    assert(false, 'Background should have a test');
+  });
+});

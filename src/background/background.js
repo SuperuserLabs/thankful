cd40567ec4852fa71122d6af9dc78ce11eacd05a
Johannes Ahnlide <johannes@voxel.se>
2018-06-12 17:20:39 +0200

    Started refactoring for creator messages

--- src/background/background.js
+++ src/background/background.js
@@ -23,29 +23,39 @@ function isTabActive(tabInfo) {
   });
 }
 
-function heartbeat(tabInfo) {
+function addTimeToCreator(name, url, duration) {
+  // Get time spent on all tabs
+  browser.storage.local.get(['timespent']).then(result => {
+    if (!result.hasOwnProperty('timespent')) {
+      result.timespent = {};
+    }
+    // Update time spent on tab
+    if (!result.timespent.hasOwnProperty(name)) {
+      result.timespent[name] = {};
+    }
+
+    let prevDuration = result.timespent[name][url] || 0;
+    result.timespent[name][url] = prevDuration + duration;
+    console.log(result.timespent);
+
+    // Store time spent on tab
+    browser.storage.local.set({ timespent: result.timespent });
+  });
+}
+
+function heartbeat(tabInfo, contentInfo) {
   isTabActive(tabInfo).then(active => {
     if (active) {
       // TODO: When active before and now, add unaccounted time for last page before adding time for current page
       let url = tabInfo.url;
       let duration = sinceLastCall(url);
 
-      // Get time spent on all tabs
-      browser.storage.local.get(['timespent']).then(result => {
-        if (result.timespent === undefined) {
-          result.timespent = {};
-        }
-        let prevDuration = result.timespent[url] || 0;
-
-        // Update time spent on tab
-        result.timespent[url] = prevDuration + duration;
-        console.log(result.timespent);
-
-        // Store time spent on tab
-        browser.storage.local.set({ timespent: result.timespent });
-
-        rescheduleAlarm();
-      });
+      if (contentInfo.url === url) {
+        addTimeToCreator(contentInfo.creator, url, duration);
+      } else {
+        addTimeToCreator(url, url, duration);
+      }
+      rescheduleAlarm();
     } else {
       console.log('Not active, not logging');
     }
@@ -70,8 +80,17 @@ function getCurrentTab() {
 (function() {
   rescheduleAlarm();
 
+  let contentInfo = {
+    url: null,
+    creator: null,
+  };
+
+  function setContentInfo(message, sender, sendResponse) {
+    contentInfo = message;
+  }
+
   function stethoscope() {
-    getCurrentTab().then(heartbeat);
+    getCurrentTab().then(tabInfo => heartbeat(tabInfo, contentInfo));
   }
 
   browser.idle.onStateChanged.addListener(stethoscope);

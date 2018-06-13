93a38ec01582d69492948efd800391ccfebc66f8
Erik Bjäreholt <erik@bjareho.lt>
2018-06-13 15:08:04 +0200

    added classes for Activity and Creator

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,25 +1,62 @@
 import Dexie from 'dexie';
 
+let _db = undefined;
+
+export class Activity {
+  constructor(url, title, duration, creator) {
+    this.url = url;
+    this.title = title;
+    this.duration = duration;
+    this.creator = creator;
+  }
+
+  save() {
+    return _db.activity.put(this);
+  }
+}
+
+export class Creator {
+  constructor(url, name, duration, creator) {
+    this.url = url;
+    this.name = name;
+    this.duration = duration;
+    this.creator = creator;
+  }
+
+  save() {
+    return _db.creator.put(this);
+  }
+}
+
 export class Database {
   constructor() {
-    this.db = new Dexie('thankful');
-    this.db.version(1).stores({
-      activity: 'url, title, duration, creator',
-      creator: 'url, title',
+    _db = new Dexie('Thankful');
+    _db.version(1).stores({
+      activity: '&url, title, duration, creator_id',
+      creator: '++id, &url, name',
     });
+    _db.activity.mapToClass(Activity);
+    _db.creator.mapToClass(Creator);
+    this.db = _db;
   }
 
   getActivity(url) {
     return this.db.activity.get({ url: url });
   }
 
-  addActivity(url, duration) {
+  addActivity(url, duration, title, creator) {
+    // Adds a duration to a URL if activity for URL already exists,
+    // otherwise creates new Activity with the given duration.
     return this.db.activity.get({ url: url }).then(activity => {
       if (activity === undefined) {
-        activity = { url: url, duration: 0 };
+        activity = { url: url, duration: 0, title: title };
       }
       activity.duration += duration;
       return this.db.activity.put(activity);
     });
+  }
+
+  getCreator(url) {
+    return this.db.creator.get({ url: url });
   }
 }
--- src/lib/db.test.js
+++ src/lib/db.test.js
@@ -1,14 +1,23 @@
 import assert from 'assert';
-import { Database } from './db.js';
 
 import Dexie from 'dexie';
+
+/*
+ * Illegal invocation: https://github.com/axemclion/IndexedDBShim/issues/318
+import setGlobalVars from 'indexeddbshim';
+setGlobalVars();
+*/
+
 Dexie.dependencies.indexedDB = require('fake-indexeddb');
 Dexie.dependencies.IDBKeyRange = require('fake-indexeddb/lib/FDBKeyRange');
 
-describe('addActivity', () => {
+import { Database, Activity, Creator } from './db.js';
+
+describe('Activity', () => {
+  let db = new Database();
+  let url = 'https://www.youtube.com/watch?v=OkFdqqyI8y4';
+
   it('correctly adds activity', () => {
-    let db = new Database();
-    let url = 'https://youtube.com/watch?v=test';
     return db
       .addActivity(url, 13.37)
       .then(() => {
@@ -24,7 +33,49 @@ describe('addActivity', () => {
         return db.getActivity(url);
       })
       .then(activity => {
-        assert(activity.duration.toFixed(3) == 23.37);
+        expect(activity.duration).toBeCloseTo(23.37, 3);
+      });
+  });
+
+  it('correctly adds activity', () => {
+    let activity = new Activity(url, undefined, 13.37);
+    return activity
+      .save()
+      .then(() => {
+        return db.getActivity(url);
+      })
+      .then(activity => {
+        expect(activity.duration).toBeCloseTo(13.37, 3);
+        return activity;
+      })
+      .then(activity => {
+        activity.duration += 10;
+        return activity.save();
+      })
+      .then(() => {
+        return db.getActivity(url);
+      })
+      .then(activity => {
+        expect(activity.duration).toBeCloseTo(23.37, 3);
+      });
+  });
+});
+
+describe('Creator', () => {
+  let db = new Database();
+  let name = 'Bethesda Softworks';
+  let url = 'https://www.youtube.com/channel/UCvZHe-SP3xC7DdOk4Ri8QBw';
+
+  it('correctly adds creator', () => {
+    let creator = new Creator(url, name);
+    return creator
+      .save()
+      .then(() => {
+        return db.getCreator(url);
+      })
+      .then(activity => {
+        expect(creator.url).toBe(url);
+        expect(creator.name).toBe(name);
       });
   });
 });

b3c307bcd624b4e6c3e84b2e800bd0089ebcd2b8
Erik Bjäreholt <erik@bjareho.lt>
2018-06-18 15:12:06 +0200

    switched to async/await in db tests

--- src/lib/db.test.js
+++ src/lib/db.test.js
@@ -15,47 +15,26 @@ describe('Activity', () => {
   let db = new Database();
   let url = 'https://www.youtube.com/watch?v=OkFdqqyI8y4';
 
-  it('correctly adds activity', () => {
-    return db
-      .logActivity(url, 13.37)
-      .then(() => {
-        return db.getActivity(url);
-      })
-      .then(activity => {
-        expect(activity.duration).toBeCloseTo(13.37, 3);
-      })
-      .then(() => {
-        return db.logActivity(url, 10);
-      })
-      .then(() => {
-        return db.getActivity(url);
-      })
-      .then(activity => {
-        expect(activity.duration).toBeCloseTo(23.37, 3);
-      });
+  it('correctly adds activity', async () => {
+    await db.logActivity(url, 13.37);
+    let activity = await db.getActivity(url);
+    expect(activity.duration).toBeCloseTo(13.37, 3);
+
+    await db.logActivity(url, 10);
+    activity = await db.getActivity(url);
+    expect(activity.duration).toBeCloseTo(23.37, 3);
   });
 
-  it('correctly adds activity', () => {
+  it('correctly adds activity', async () => {
     let activity = new Activity(url, undefined, 13.37);
-    return activity
-      .save()
-      .then(() => {
-        return db.getActivity(url);
-      })
-      .then(activity => {
-        expect(activity.duration).toBeCloseTo(13.37, 3);
-        return activity;
-      })
-      .then(activity => {
-        activity.duration += 10;
-        return activity.save();
-      })
-      .then(() => {
-        return db.getActivity(url);
-      })
-      .then(activity => {
-        expect(activity.duration).toBeCloseTo(23.37, 3);
-      });
+    await activity.save();
+    activity = await db.getActivity(url);
+    expect(activity.duration).toBeCloseTo(13.37, 3);
+
+    activity.duration += 10;
+    await activity.save();
+    activity = await db.getActivity(url);
+    expect(activity.duration).toBeCloseTo(23.37, 3);
   });
 });
 
@@ -66,54 +45,35 @@ describe('Creator', () => {
   let a_title = 'Elder Scrolls 6 Trailer';
   let a_url = 'https://www.youtube.com/watch?v=OkFdqqyI8y4';
 
-  it('get all creators', () => {
-    return new Creator(c_url, c_name)
-      .save()
-      .then(() => new Creator('testurl', 'testname').save())
-      .then(() => {
-        return db.getCreators();
-      })
-      .then(creators => {
-        expect(creators).toHaveLength(2);
-      });
+  it('get all creators', async () => {
+    await new Creator(c_url, c_name).save();
+    await new Creator('testurl', 'testname').save();
+
+    let creators = await db.getCreators();
+    expect(creators).toHaveLength(2);
   });
 
-  it('correctly adds creator', () => {
-    return new Creator(c_url, c_name)
-      .save()
-      .then(_ => {
-        return db.getCreator(c_url);
-      })
-      .then(creator => {
-        expect(creator.url).toBe(c_url);
-        expect(creator.name).toBe(c_name);
-        return creator;
-      });
+  it('correctly adds creator', async () => {
+    await new Creator(c_url, c_name).save();
+
+    let creator = await db.getCreator(c_url);
+    expect(creator.url).toBe(c_url);
+    expect(creator.name).toBe(c_name);
   });
 
-  it('connect activity to creator', () => {
+  it('connect activity to creator', async () => {
+    let key = await new Creator(c_url, c_name).save();
+
+    // Test fetching creator by key
+    let creator = await db.getCreator(key);
+    expect(creator.url).toBe(c_url);
+    expect(creator.name).toBe(c_name);
+
     let activity = new Activity(a_url, a_title);
-    return new Creator(c_url, c_name)
-      .save()
-      .then(key => {
-        return db.getCreator(key);
-      })
-      .then(creator => {
-        expect(creator.url).toBe(c_url);
-        expect(creator.name).toBe(c_name);
-        return creator;
-      })
-      .then(_ => {
-        return activity.save();
-      })
-      .then(_ => {
-        return db.connectActivityToCreator(activity.url, c_url);
-      })
-      .then(_ => {
-        return db.getCreatorActivity(c_url);
-      })
-      .then(creatorActivity => {
-        expect(creatorActivity).toHaveLength(1);
-      });
+    await activity.save();
+    await db.connectActivityToCreator(activity.url, c_url);
+
+    let creatorActivity = await db.getCreatorActivity(c_url);
+    expect(creatorActivity).toHaveLength(1);
   });
 });

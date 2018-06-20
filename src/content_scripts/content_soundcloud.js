d46f0824af7f11c3618f54068fba7d21aa7db839
Johannes Ahnlide <johannes@voxel.se>
2018-06-20 12:01:37 +0200

    Fixed wrong name bug

--- src/content_scripts/content_soundcloud.js
+++ src/content_scripts/content_soundcloud.js
@@ -10,7 +10,7 @@ function crawlPage() {
   waitForElement('.playbackSoundBadge__titleContextContainer', 1000).then(
     soundbadge => {
       let url = document.location.href;
-      let links = ownerContainer.getElementsByTagName('a'); // Song is at index 0, artist at index 1
+      let links = soundbadge.getElementsByTagName('a'); // Song is at index 0, artist at index 1
       let creator = {
         id: links[1].getAttribute('href'),
         name: links[1].innerText,

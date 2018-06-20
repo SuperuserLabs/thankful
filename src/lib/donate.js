a17818af804b0eb12cb16e375ee90ca173c54eb9
Jacob Karlsson <jacob.karlsson95@gmail.com>
2018-06-20 12:50:40 +0200

    Communicate with metaemask and send donations

--- src/lib/donate.js
+++ src/lib/donate.js
@@ -1,73 +1,90 @@
+import Web3 from 'web3';
+import MetamaskInpageProvider from 'metamask-crx/app/scripts/lib/inpage-provider.js';
+import PortStream from 'metamask-crx/app/scripts/lib/port-stream.js';
+
 const addr = {};
 // All on Ropsten
 addr.erik = '0xbD2940e549C38Cc6b201767a0238c2C07820Ef35';
 addr.patrik = '0xbcEf85708670FA0127C484Ac649724B8028Ea08b';
 addr.jacob = '0xBF9e8395854cE02abB454d5131F45F2bFFB54017';
 
+let web3;
+
 export default class Donate {
   constructor() {
     console.log('in donate constructor');
-    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-    if (typeof web3 !== 'undefined') {
-      // Use Mist/MetaMask's provider
-      console.log('Using metamask');
-      web3 = new Web3(web3.currentProvider);
-    } else {
-      throw 'Found no injected web3. Have you logged into Metamask?';
-      // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-      // We'll probably want to use this for unit testing
-      // web3js = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-    }
 
-    web3.version.getNetwork((err, netId) => {
-      switch (netId) {
-        case '1':
-          console.log('This is mainnet');
-          break;
-        case '2':
-          console.log('This is the deprecated Morden test network.');
-          break;
-        case '3':
-          console.log('This is the ropsten test network.');
-          break;
-        case '4':
-          console.log('This is the Rinkeby test network.');
-          break;
-        case '42':
-          console.log('This is the Kovan test network.');
-          break;
-        default:
-          console.log('This is an unknown network.');
+    const METAMASK_EXTENSION_ID = 'nkbihfbeogaeaoehlefnkodbefgpgknn';
+    // TODO: Change to browser.
+    const metamaskPort = chrome.runtime.connect(METAMASK_EXTENSION_ID);
+    const pluginStream = new PortStream(metamaskPort);
+    const web3Provider = new MetamaskInpageProvider(pluginStream);
+    web3 = new Web3(web3Provider);
+
+    // We might want to use this for unit testing
+    // web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+
+    console.log('web3:', web3);
+
+    web3.eth.net.getId((err, netId) => {
+      if (err) {
+        throw 'Could not get network ID';
       }
+      // Networks:
+      // 1:  Mainnet
+      // 2:  Deprecated Morden testnet
+      // 3:  Ropsten testnet
+      // 4:  Rinkeby testnet
+      // 42: Kovan testnet
+      if (netId !== 3) {
+        // Abort if not connected to Ropsten (testnet)
+        web3 = undefined;
+        throw 'Not connected to Ropsten, connected to: ' + netId;
+      }
+      console.log('Connected to Ropsten');
     });
 
     console.log('Connected to web3');
+  }
+
+  _accountsPromise() {
+    return Promise((resolve, reject) => {
+      web3.eth.getAccounts((err, accounts) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(accounts);
+        return;
+      });
+    });
   }
 
   donateAll() {
     // TODO: use the web3 initialized above
     // Async is required here
     // https://github.com/MetaMask/faq/blob/master/DEVELOPERS.md#dizzy-all-async---think-of-metamask-as-a-light-client
-    web3.eth.sendTransaction(
-      {
-        from: _myAcc(),
-        to: addr.erik,
-        value: 1e15,
-        gas: 1e6,
-      },
-      (err, res) => {
-        if (err) {
-          throw err;
+    this._myAcc(acc => {
+      web3.eth.sendTransaction(
+        {
+          from: acc,
+          to: addr.erik,
+          value: 1e16,
+          gas: 1e6,
+        },
+        (err, res) => {
+          if (err) {
+            throw err;
+          }
+          console.log('Sent money:', res);
         }
-        console.log('Sent money:', res);
-      }
-    );
+      );
+    });
   }
 
-  _myAcc() {
-    // Sync is supposed to work here but didn't
-    web3.eth.getAccounts(function(err, accounts) {
-      return console.log('My account:', accounts[0]);
+  _myAcc(callback) {
+    web3.eth.getAccounts((err, accounts) => {
+      callback(accounts[0]);
     });
   }
 }

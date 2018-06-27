eb6e35985b4bfb719ea4420262d0103f7a7e0023
Erik Bjäreholt <erik@bjareho.lt>
2018-06-27 09:49:22 +0200

    fixed typing

--- allocate.py
+++ allocate.py
@@ -1,24 +1,31 @@
 from datetime import timedelta
 from collections import defaultdict
+from typing import Dict
 
 
 class Creator(dict):
-    def __init__(self, name, *args, **kwargs):
+    def __init__(self, name, *args,
+                 timespent: timedelta = timedelta(0),
+                 rating: float = 0.0,
+                 donations: dict = dict(),
+                 **kwargs) -> None:
         self.name = name
-        assert "rating" in kwargs
+        self.timespent = timespent
+        self.rating = rating
+        self._donations = donations
         dict.__init__(self, *args, **kwargs)
 
-    def expected_costs(self):
+    def expected_costs(self) -> float:
         # Assume everyone has costs of $1000/mo for now
         return 1000
 
-    def expected_earnings(self):
+    def expected_earnings(self) -> float:
         return round(self.expected_revenue() - self.expected_costs())
 
-    def expected_revenue(self):
+    def expected_revenue(self) -> float:
         return round(self._expected_revenue_ads() + self._expected_revenue_donations())
 
-    def _expected_revenue_ads(self, cpm=0.25):
+    def _expected_revenue_ads(self, cpm=0.25) -> float:
         """
             A conservative estimate of monthly income from ads
              - https://socialblade.com/youtube/youtube-money-calculator
@@ -27,44 +34,36 @@ class Creator(dict):
             return 31 * cpm * self["daily_views"] / 1e4
         return 0
 
-    def _expected_revenue_donations(self):
+    def _expected_revenue_donations(self) -> float:
         """
             A highly speculative calculator of expected monhtly income from donations
         """
         subs = self.subscribers
-        income = 0
+        income = 0.0
         if "YouTube" in subs:
             income += subs.pop("YouTube") * 0.01  # A YouTuber should be able to get 0.01$ in donations per subscriber, on average
         income += sum(subs.values()) * 0.001  # Subscribers on other platforms of unknown value
         income += self.donations["USD"]
         return income
 
-    def weight(self, total_timespent=None):
+    def weight(self, total_timespent=None) -> float:
         timespent_frac = (self.timespent.total_seconds() / total_timespent.total_seconds()) \
             if total_timespent else 1
         weight = (2 * max(0, self.rating - 0.5)) * timespent_frac
         return round(weight, 3)
 
     @property
-    def timespent(self):
-        return self["timespent"] if "timespent" in self else timedelta(0)
-
-    @property
-    def rating(self):
-        return self["rating"]
-
-    @property
-    def donations(self):
+    def donations(self) -> Dict[str, float]:
         # Can include:
         #   - Average monthly Patreon donations for creators
         #   - Salary for employees
-        donations = defaultdict(lambda: 0)
-        donations.update(self["donations"] if "donations" in self else {})
+        donations: Dict[str, float] = defaultdict(lambda: 0.0)
+        donations.update(self._donations)
         return donations
 
     @property
-    def subscribers(self):
-        subscribers = defaultdict(lambda: 0)
+    def subscribers(self) -> Dict[str, float]:
+        subscribers: Dict[str, float] = defaultdict(lambda: 0)
         subscribers.update(self["subscribers"] if "subscribers" in self else {})
         return subscribers
 
@@ -87,6 +86,7 @@ def main():
         print(f"{creator.name:12} (w: {creator.weight():3},  donations: {dict(creator.donations)})")
         print(f" - E(revenue):   ${creator.expected_revenue()}")
         print(f" - E(earnings):  ${creator.expected_earnings()}")
+
         allocated = round(monthly_budget * creator.weight() / total_weight, 1)
         print(f" - Allocated:    ${allocated}\n")
 

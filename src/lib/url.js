97aab698a24b648aaed426070bf5c7009c4e867a
Johan Bjäreholt <johan@bjareho.lt>
2018-07-05 15:45:01 +0200

    Only canonicalize youtube URLs

--- src/lib/url.js
+++ src/lib/url.js
@@ -4,29 +4,16 @@ export function isOnDomain(url, domain) {
   return reg.test(urla.hostname);
 }
 
-export function canonicalizeUrl(orig_url_str) {
-  // Base format is: host
-  // example: google.com
-  let url = new URL(orig_url_str);
-  let url_str = url.host;
-
-  // Extension formats for different domains
+export function canonicalizeUrl(url_str) {
+  let url = new URL(url_str);
   if (url.host == 'www.youtube.com') {
-    // pathname+searchParams(v)
-    // example: /watch?v=videoid
+    // example: www.youtube.com/watch?v=videoid
+    // removes searchParams such as t=1min, feature=youtu.be etc.
+    url_str = url.origin;
     url_str += url.pathname;
     if (url.pathname == '/watch') {
       url_str += '?v=' + url.searchParams.get('v');
     }
-  } else if (url.host == 'github.com') {
-    // first depth path
-    // example: /SuperuserLabs
-    let levels = url.pathname.split('/');
-    console.log(levels);
-    if (levels.length > 0) {
-      url_str += '/' + levels[1];
-    }
   }
-  console.log(url_str);
   return url_str;
 }

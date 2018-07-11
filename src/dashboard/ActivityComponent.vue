e4453472bb6426c3bb97b48bb0f999466b8ebd7a
Patrik Laurell <patrik.laurell@gmail.com>
2018-07-11 12:59:05 +0200

    redesign of activities table

--- src/dashboard/ActivityComponent.vue
+++ src/dashboard/ActivityComponent.vue
@@ -1,41 +1,54 @@
 <template lang="pug">
-div
-  v-data-table(:headers="headers", :items="activities", :pagination.sync='pagination', hide-actions)
-    template(slot='items', slot-scope='props')
-      td
+div(v-if="activities.length === 0")
+  | No activity logged
+div(v-else)
+  v-data-table(:headers="headers", :items="orderedActivities", style="width:100%", hide-actions="showAll")
+    template(slot='items', slot-scope='props', style="width:100%")
+      td(style="text-overflow: ellipsis;")
         a(:href="props.item.url", target="_blank")
           | {{ props.item.title || props.item.url }}
       td.text-right
         | {{ props.item.duration | friendlyDuration }}
   div.text-xs-center.pt-2
-    v-btn(v-if="toAll", size="sm", :to="toAll")
+    v-btn(v-if="!showAll", size="sm", :to="toAll")
       | Show all
 </template>
 
 <script>
+import { Database } from '../lib/db.js';
+import _ from 'lodash';
+
+const db = new Database();
+
 export default {
-  data: () => ({
-    activities: [],
-    headers: [
-      { text: 'Page', value: 'title' },
-      { text: 'Duration', value: 'duration' },
-    ],
-    pagination: { sortBy: 'duration', descending: true, rowsPerPage: 10 },
-  }),
+  data: function() {
+    return {
+      activities: [],
+      headers: [
+        { text: 'Page', align: 'left' },
+        { text: 'Duration', align: 'left' },
+      ],
+    };
+  },
   props: {
     limit: { default: Infinity, type: Number },
     unattributed: { default: false, type: Boolean },
-    toAll: { default: null, type: String },
+    to: { default: null, type: String },
+    showAll: { default: false, type: Boolean },
   },
-  computed: {},
+  computed: {
+    orderedActivities() {
+      return _.take(_.orderBy(this.activities, 'duration', 'desc'), this.limit);
+    },
+  },
   methods: {
     refresh() {
       if (this.unattributed) {
-        this.$db.getActivities({ withCreators: false }).then(acts => {
+        db.getActivities({ withCreators: false }).then(acts => {
           this.activities = acts;
         });
       } else {
-        this.$db.getActivities().then(acts => {
+        db.getActivities().then(acts => {
           this.activities = acts;
         });
       }

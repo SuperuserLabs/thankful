b70118ba253d9c2b3fcf817871f0a3217c2eadab
Valthor Halldorsson <halldorsson.v@gmail.com>
2018-07-19 14:40:51 +0200

    Switch to a less build-time costly source-map type

--- webpack.config.js
+++ webpack.config.js
@@ -87,5 +87,5 @@ module.exports = {
       NODE_ENV: 'development', // use 'development' unless process.env.NODE_ENV is defined
     }),
   ],
-  devtool: 'source-map',
+  devtool: 'cheap-module-eval-source-map',
 };

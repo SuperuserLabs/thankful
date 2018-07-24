9a3ec03f56bd08ead499713d9073aa3025b53445
Johannes Ahnlide <johannes@voxel.se>
2018-07-24 15:14:56 +0200

    Better imports, lazy db init in popup

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,19 +1,22 @@
 import Vue from 'vue';
-import Vuetify from 'vuetify';
-
-import { Database } from '../lib/db.js';
+import {
+  Vuetify, // required
+  VApp, // required
+  VBtn,
+} from 'vuetify';
 
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
 Vue.use(Vuetify, {
-  theme: {},
+  components: {
+    VApp,
+    VBtn,
+  },
 });
 
 import App from './popup.vue';
 
 import 'vuetify/dist/vuetify.min.css';
-
-Vue.prototype.$db = new Database();
 
 new Vue({
   el: '#popup',

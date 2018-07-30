f065c8d53865960e4d899ec4b71de6ec2d3aa278
Johannes Ahnlide <johannes@voxel.se>
2018-07-30 18:09:25 +0200

    Fixed review comments

--- src/content_scripts/content_medium.js
+++ src/content_scripts/content_medium.js
@@ -9,20 +9,17 @@ import {
 
 function crawlPage() {
   // Tries to extract channel URL from page, retries after 1 second if not successful.
-  waitForElements(['meta[property=author]', 'link[rel=author]'], 1000)
-    .then(meta => {
-      let url = document.location.href;
-      let [c_name, c_url] = meta;
-      let creator = new Creator(c_url.href, c_name.content);
-      console.log('Found creator: ' + JSON.stringify(creator));
-      sendCreator(url, creator);
-    })
-    .catch(err => console.log(err));
+  return waitForElements(
+    ['meta[property=author]', 'link[rel=author]'],
+    1000
+  ).then(([c_name, c_url]) => {
+    let url = document.location.href;
+    let creator = new Creator(c_url.href, c_name.content);
+    sendCreator(url, creator);
+  });
 }
 
 (function() {
-  console.log('crawlPage defined');
-
   crawlPage();
 
   addPageChangeListener(crawlPage);
--- src/content_scripts/content_youtube.js
+++ src/content_scripts/content_youtube.js
@@ -9,28 +9,20 @@ import {
 
 function crawlPage() {
   // Tries to extract channel URL from page, retries after 1 second if not successful.
-  waitForElement('div#owner-container a', 1000)
-    .then(channelLink => {
-      let url = document.location.href;
-      if (channelLink === undefined) {
-        // TODO: Try again
-        throw 'channelLink was undefined';
-      }
-      let c_url = channelLink.getAttribute('href');
-      if (!c_url.includes('://')) {
-        c_url = 'https://www.youtube.com' + c_url;
-      }
-      let c_name = channelLink.textContent;
-      let creator = new Creator(c_url, c_name);
-      console.log('Found creator: ' + JSON.stringify(creator));
-      sendCreator(url, creator);
-    })
-    .catch(err => console.log(err));
+  return waitForElement('div#owner-container a', 1000).then(channelLink => {
+    let url = document.location.href;
+    let c_url = channelLink.getAttribute('href');
+    if (!c_url.includes('://')) {
+      c_url = 'https://www.youtube.com' + c_url;
+    }
+    let c_name = channelLink.textContent;
+    let creator = new Creator(c_url, c_name);
+    console.log('Found creator: ' + JSON.stringify(creator));
+    sendCreator(url, creator);
+  });
 }
 
 (function() {
-  console.log('crawlPage defined');
-
   crawlPage();
 
   addPageChangeListener(crawlPage);

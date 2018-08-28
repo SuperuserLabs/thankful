06b8e8a0c047452973cd167280c07fa3763b50d1
Jacob Karlsson <jacob.karlsson95@gmail.com>
2018-08-28 15:52:40 +0200

    Use metamask-extension-provider
    Only works in chrome though lol

--- src/lib/donate.js
+++ src/lib/donate.js
@@ -1,38 +1,26 @@
 import BigNumber from 'bignumber.js';
-import { isWebExtension } from './util';
-import browser from 'webextension-polyfill';
-
-const addrs = {};
-// All on Ropsten
-addrs.erik = '0xbD2940e549C38Cc6b201767a0238c2C07820Ef35';
-// Forgot his password?
-//addrs.patrik = '0xbcEf85708670FA0127C484Ac649724B8028Ea08b';
-addrs.jacob = '0xBF9e8395854cE02abB454d5131F45F2bFFB54017';
-addrs.johan = '0xB41371729C8e5EEF5D0992f8c2D10f809EcFE112';
-addrs.johannes = '0x4D69bbD5417aB826F9DAbc7BBb6ddE60C5c5EF26';
 
 let web3;
 
 export default class Donate {
-  init() {
-    return this._metamaskExtensionId().then(async METAMASK_EXTENSION_ID => {
-      const Web3 = (await import('web3')).default;
-      const MetamaskInpageProvider = (await import('metamask-crx/app/scripts/lib/inpage-provider.js'))
-        .default;
-      const PortStream = (await import('metamask-crx/app/scripts/lib/port-stream.js'))
-        .default;
-      const metamaskPort = browser.runtime.connect(METAMASK_EXTENSION_ID);
-      const pluginStream = new PortStream(metamaskPort);
-      const web3Provider = new MetamaskInpageProvider(pluginStream);
-      await import('bn.js');
-      web3 = new Web3(web3Provider);
+  async init() {
+    const Web3 = (await import('web3')).default;
+    const createMetaMaskProvider = (await import('metamask-extension-provider'))
+      .default;
+    const web3Provider = createMetaMaskProvider();
 
-      // We might want to use this for unit testing
-      // web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
-
-      return web3.eth.net.getId();
+    web3Provider.on('error', error => {
+      // Failed to connect to MetaMask, fallback logic.
+      console.error('Failed to connect to MetaMask:', error);
     });
+
+    await import('bn.js');
+    web3 = new Web3(web3Provider);
+    console.log('created web3');
+
+    return web3.eth.net.getId();
   }
+
   async getId() {
     return web3.eth.net.getId();
   }
@@ -112,26 +100,6 @@ export default class Donate {
     return web3.eth.getAccounts().then(accounts => {
       return accounts[0];
     });
-  }
-
-  _metamaskExtensionId() {
-    if (isWebExtension()) {
-      if (browser.runtime.getBrowserInfo) {
-        return browser.runtime.getBrowserInfo().then(res => {
-          if (res.name === 'Firefox') {
-            return 'webextension@metamask.io';
-          } else {
-            // Assume Chrome if it's some other string
-            return 'nkbihfbeogaeaoehlefnkodbefgpgknn';
-          }
-        });
-      } else {
-        // Assume Chrome if getBrowserInfo isn't defined
-        return Promise.resolve('nkbihfbeogaeaoehlefnkodbefgpgknn');
-      }
-    } else {
-      return Promise.reject('Not supported outside WebExtension, yet');
-    }
   }
 
   async _usdEthRate() {

c67e099c59ec47a45896694f15c0b6147f28fce7
Dwight Lidman <dwight.lidman@gmail.com>
2019-06-25 11:32:49 +0200

    donation buttons

--- src/dashboard/components/OnboardingDonate.vue
+++ src/dashboard/components/OnboardingDonate.vue
@@ -1,6 +1,31 @@
 <template lang="pug">
 div
-  p How much donut pls?
+  span At the end of each month, the amount you set below will be distributed to creators you love.
+
+  input(type="radio", id="one", value="One", v-model="picked")
+  label(for="one") One
+  input(type="radio", id="two", value="Two", v-model="picked")
+  label(for="two") Two
+  p Picked:
+  | {{ picked }}
+
+  v-layout(row)
+    v-flex.px-4
+      v-btn-toggle(v-model="donation_amount", style="width: 100%")
+        v-btn(value="10", block) 10
+    v-flex.px-4
+      v-btn-toggle(v-model="donation_amount", style="width: 100%")
+        v-btn(value="20", block) 20
+    v-flex.px-4
+      v-btn-toggle(v-model="donation_amount", style="width: 100%")
+        v-btn(value="50", block) 50
 </template>
 
-<script></script>
+<script>
+export default {
+  data: () => ({
+    picked: 'One',
+    donation_amount: 1,
+  }),
+};
+</script>
--- src/dashboard/components/OnboardingWelcome.vue
+++ src/dashboard/components/OnboardingWelcome.vue
@@ -1,8 +1,9 @@
 <template lang="pug">
 div
   h1 Thank you for using Thankful!
-  p We're going to briefly explain how Thankful works and get you up and running.
-  router-link(to="/onboarding/thankful")
+  p Thankful will keep track of creators whose content you view via your browser activity and then we'll get back to you at the end of the month, when it's time to send out your donations.
+  p But first, we need to go through a few short steps and set things up.
+  router-link(to="/onboarding/donate")
     v-btn(color="primary") Get started
 </template>
 

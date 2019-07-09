e6635524ed15e13a65dd32a51a9d170a3c745954
Erik Bjäreholt <erik@bjareho.lt>
2019-07-09 19:07:00 +0200

    added text showing time since last donation on dashboard

--- src/dashboard/components/DonationSummary.vue
+++ src/dashboard/components/DonationSummary.vue
@@ -53,9 +53,7 @@ div.pt-2
 
 <script>
 import _ from 'lodash';
-import moment from 'moment';
 import { mapState, mapGetters } from 'vuex';
-import { getInstallDate } from '../../lib/util.ts';
 
 export default {
   props: ['checkout'],
@@ -78,7 +76,6 @@ export default {
           v => !v || this.isAddress(v) || 'Not a valid ETH address',
         ],
       },
-      lastDonationDate: new Date(),
     };
   },
   computed: {
@@ -95,12 +92,6 @@ export default {
         return 'Please log in to MetaMask to be able to donate';
       }
       return '';
-    },
-    timeSinceLastDonation() {
-      const last = this.lastDonationDate;
-      const time_since = moment().diff(last) / 1000;
-      console.log('last donation was', last, ', ', time_since, 'seconds ago');
-      return time_since;
     },
     monthlyAmount() {
       const one_month = 60 * 60 * 24 * 30; // 30 days in seconds
@@ -143,17 +134,9 @@ export default {
         };
       });
     },
-    async updateLastDonationDate() {
-      const lastDonation = this.$store.state.db.donations[0];
-      this.lastDonationDate =
-        lastDonation === undefined
-          ? await getInstallDate()
-          : new Date(lastDonation.date);
-    },
   },
   async created() {
     await this.$store.dispatch('db/ensureDonations');
-    await this.updateLastDonationDate();
     this.distribute();
   },
   watch: {

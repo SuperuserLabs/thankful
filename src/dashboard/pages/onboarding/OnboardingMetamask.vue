a1c00bea20b8ae01b54f7d3bf2bab17a10a3e1e8
Jacob Karlsson <jacob.karlsson95@gmail.com>
2019-07-12 16:03:11 +0200

    Switch to v-alert

--- src/dashboard/pages/onboarding/OnboardingMetamask.vue
+++ src/dashboard/pages/onboarding/OnboardingMetamask.vue
@@ -13,7 +13,7 @@ v-stepper(v-model="step", vertical)
     | Log in to MetaMask
   v-stepper-content(step=2)
     b Now open MetaMask to log in to your account.
-    div(class="note") #[b NOTE:] Unfortunately, due to (temporary) #[a(href='https://github.com/MetaMask/metamask-extension/issues/5950') technical reasons], you also need to #[b turn off privacy mode] in MetaMask (#[a(href='https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8') details on privacy mode]). You can do this by:
+    v-alert.note(:value="true" type="info" outline) Unfortunately, due to (temporary) #[a(href='https://github.com/MetaMask/metamask-extension/issues/5950') technical reasons], you also need to #[b turn off privacy mode] in MetaMask (#[a(href='https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8') details on privacy mode]). You can do this by:
       ol
         li Opening MetaMask
         li Clicking on your user icon in the top right
@@ -33,11 +33,6 @@ v-stepper(v-model="step", vertical)
 <style scoped>
 .note {
   margin: 1rem 3rem;
-  padding: 1rem;
-  border-radius: 5px;
-  border-style: solid;
-  border-color: #bbb;
-  border-width: 2px;
 }
 
 /*

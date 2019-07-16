6ccbcece2a1ed17ca785cbbf6a0d609b971f2fe5
Erik Bjäreholt <erik@bjareho.lt>
2019-07-16 19:34:12 +0200

    moved creator URL links to seperate component and included it in DonationSummary and CreatorList

--- src/dashboard/components/CreatorCard.vue
+++ src/dashboard/components/CreatorCard.vue
@@ -4,10 +4,7 @@ v-card(height='116px').mt-1
     div(style="text-overflow: ellipsis; overflow-x: hidden; width: 100%; text-align: center;")
       h3.mb-0 {{ name }}
     div(style="margin: 0 auto;")
-      span(v-for="site in sites")
-        a.title(v-if='site.url', :href='site.url' target="_blank" style="text-decoration: none !important")
-          v-btn.ma-0(icon, style="font-size: inherit")
-            font-awesome-icon(:icon="site.icon", :color='site.color')
+      t-creator-sites(:urls='urls')
   v-card-actions.pt-0
     v-layout(row, align-center).ma-0.pl-1.body-1.text--secondary
       span(v-if="duration").pr-1
@@ -64,12 +61,14 @@ v-card(height='116px').mt-1
 </template>
 
 <script>
-import { isOnDomain } from '~/lib/url.ts';
-import { find as _find, sortBy } from 'lodash';
+import CreatorSites from './CreatorSites.vue';
 
 export default {
   data() {
     return this.setDefaultData({});
+  },
+  components: {
+    't-creator-sites': CreatorSites,
   },
   props: {
     creator: Object,
@@ -79,61 +78,7 @@ export default {
       this.$emit('allocatedFunds', to);
     },
   },
-  computed: {
-    sites() {
-      let platforms = [
-        {
-          domain: 'getthankful.io',
-          icon: ['fas', 'star'],
-          color: '#FFCC44',
-        },
-        {
-          domain: 'youtube.com',
-          icon: ['fab', 'youtube'],
-          color: 'red',
-        },
-        {
-          domain: 'github.com',
-          icon: ['fab', 'github'],
-          color: 'black',
-        },
-        {
-          domain: 'twitter.com',
-          icon: ['fab', 'twitter'],
-          color: '#38A1F3',
-        },
-        {
-          domain: 'medium.com',
-          icon: ['fab', 'medium'],
-          color: 'black',
-        },
-        {
-          domain: 'patreon.com',
-          icon: ['fab', 'patreon'],
-          color: 'rgb(232, 91, 70)',
-        },
-      ];
-
-      let fallback = {
-        domain: 'fallback',
-        icon: ['fas', 'globe'],
-        color: '#4AF',
-      };
-
-      let sites = this.urls.map(url => {
-        let matchedPlatform = _find(platforms, p => isOnDomain(url, p.domain));
-        if (matchedPlatform) {
-          return Object.assign({ url: url }, matchedPlatform);
-        } else {
-          return Object.assign({ url: url }, fallback);
-        }
-      });
-      sites = sortBy(sites, ['domain']);
-      return sites;
-    },
-  },
   methods: {
-    isOnDomain: isOnDomain,
     setDefaultData(obj) {
       return Object.assign(obj, {
         address: this.creator.address || '',

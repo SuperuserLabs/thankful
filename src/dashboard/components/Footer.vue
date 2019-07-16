ab6e32bf51cb39a19311f0d2dec1997ac6b98adb
Erik Bjäreholt <erik@bjareho.lt>
2019-07-16 15:57:05 +0200

    minor fixes to footer

--- src/dashboard/components/Footer.vue
+++ src/dashboard/components/Footer.vue
@@ -1,10 +1,9 @@
 <template lang="pug">
-v-footer(height="auto") 
-  v-card.text-xs-center.m-0(flat, tile, style="width: 100%")
+v-footer(height="auto")
+  v-card.text-xs-center.m-0(flat, tile, style="width: 100%; background-color: #EEE")
     v-card-text.pa-2
       v-btn(v-for="link in links" :key="link.text" flat :href="link.link" :icon="link.icon" target="_blank" large)
-        font-awesome-icon(v-if="link.icon === 'globe'", :icon="['fas', link.icon]")
-        font-awesome-icon(v-else, :icon="['fab', link.icon]")
+        font-awesome-icon(:icon="link.icon")
     v-divider
     v-card-text(py-3 text-xs-center xs12)
       | © MPL-2.0 — <strong>Thankful</strong>
@@ -17,22 +16,22 @@ export default {
       links: [
         {
           text: 'Website',
-          icon: 'globe',
+          icon: ['fas', 'globe'],
           link: 'https://getthankful.io',
         },
         {
           text: 'Twitter',
-          icon: 'twitter',
+          icon: ['fab', 'twitter'],
           link: 'https://twitter.com/getthankful',
         },
         {
           text: 'Facebook',
-          icon: 'facebook-f',
+          icon: ['fab', 'facebook-f'],
           link: 'https://www.facebook.com/Thankful-2303002679777110/',
         },
         {
           text: 'GitHub',
-          icon: 'github',
+          icon: ['fab', 'github'],
           link: 'https://github.com/SuperuserLabs/thankful/',
         },
       ],

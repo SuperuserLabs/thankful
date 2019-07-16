17eba5e53cce85069fc1ee966200a6e8f4482b48
Patrik Laurell <patrik.laurell@gmail.com>
2019-07-16 14:49:31 +0200

    fixed footer styling

--- src/dashboard/components/Footer.vue
+++ src/dashboard/components/Footer.vue
@@ -1,10 +1,12 @@
 <template lang="pug">
-v-footer 
-  v-layout(justify-center row wrap)
-    v-btn(v-for="link in links" :key="link.text" flat :href="link.link" :icon="link.icon" target="_blank" large)
-      | {{ link.icon ? '' : link.text }}
-      font-awesome-icon(:icon="['fab', link.icon]")
-    v-flex(py-3 text-xs-center xs12)
+v-footer(height="auto") 
+  v-card.text-xs-center.m-0(flat, tile, style="width: 100%")
+    v-card-text
+      v-btn(v-for="link in links" :key="link.text" flat :href="link.link" :icon="link.icon" target="_blank" large)
+        | {{ link.icon ? '' : link.text }}
+        font-awesome-icon(:icon="['fab', link.icon]")
+    v-divider
+    v-card-text(py-3 text-xs-center xs12)
       | © MPL-2.0 — <strong>Thankful</strong>
 </template>
 

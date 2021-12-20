8e8520f2e939c6e92ccc868df51e6a2cf23cd031
Erik Bjäreholt <erik@bjareho.lt>
2021-12-20 09:55:12 +0100

    fix: progress on vue-cli migration

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,44 @@
+/*eslint-env node*/
+
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
-}
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          esmodules: true,
+        },
+      },
+    ],
+    '@babel/typescript',
+  ],
+
+  plugins: [
+    'lodash',
+    '@babel/plugin-transform-runtime',
+    [
+      'transform-imports',
+      {
+        vuetify: {
+          transform: 'vuetify/es5/components/${member}',
+          preventFullImport: true,
+        },
+        lodash: {
+          transform: 'lodash/${member}',
+          preventFullImport: true,
+        },
+        '@fortawesome/free-solid-svg-icons': {
+          transform: '@fortawesome/free-solid-svg-icons/${member}',
+          skipDefaultConversion: true,
+        },
+        '@fortawesome/free-brands-svg-icons': {
+          transform: '@fortawesome/free-brands-svg-icons/${member}',
+          skipDefaultConversion: true,
+        },
+      },
+    ],
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-syntax-dynamic-import',
+    '@vue/cli-plugin-babel/preset',
+  ],
+};
